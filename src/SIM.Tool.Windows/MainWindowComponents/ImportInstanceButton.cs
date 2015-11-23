40ffbab0eb9d54192640838e4f5cbbfad6e67ca6
AlenPelin <AlenPelin@sitecore.net>
2015-11-23 16:57:33 +1100

    Improved error message in ImportInstanceButton which closes #44.

--- src/SIM.Tool.Windows/MainWindowComponents/ImportInstanceButton.cs
+++ src/SIM.Tool.Windows/MainWindowComponents/ImportInstanceButton.cs
@@ -2,10 +2,12 @@
 {
   using System.Windows;
   using Microsoft.Win32;
+  using SIM.FileSystem;
   using SIM.Instances;
   using SIM.Tool.Base;
   using SIM.Tool.Base.Plugins;
   using SIM.Tool.Wizards;
+  using Sitecore.Diagnostics.Base;
   using Sitecore.Diagnostics.Base.Annotations;
 
   [UsedImplicitly]
@@ -13,33 +15,55 @@
   {
     #region Public methods
 
-    public bool IsEnabled(Window mainWindow, Instance instance)
+    public bool IsEnabled([CanBeNull] Window mainWindow, Instance instance)
     {
       return true;
     }
 
     public void OnClick(Window mainWindow, Instance instance)
     {
+      Assert.ArgumentNotNull(mainWindow, "mainWindow");
+
       var fileDialog = new OpenFileDialog
       {
-        Title = "Select zip file of exported solution", 
-        Multiselect = false, 
+        Title = "Select zip file of exported solution",
+        Multiselect = false,
         DefaultExt = ".zip"
       };
 
       fileDialog.ShowDialog();
-      string filePath = fileDialog.FileName;
-      if (!string.IsNullOrEmpty(filePath))
+      var filePath = fileDialog.FileName;
+      if (string.IsNullOrEmpty(filePath))
       {
-        if (FileSystem.FileSystem.Local.Zip.ZipContainsFile(filePath, "AppPoolSettings.xml") && FileSystem.FileSystem.Local.Zip.ZipContainsFile(filePath, "WebsiteSettings.xml") && FileSystem.FileSystem.Local.Zip.ZipContainsFile(filePath, @"Website/Web.config"))
-        {
-          WizardPipelineManager.Start("import", mainWindow, null, null, MainWindowHelper.SoftlyRefreshInstances, filePath);
-        }
-        else
-        {
-          WindowHelper.ShowMessage("Wrong package for import.");
-        }
+        return;
       }
+
+      const string AppPoolFileName = "AppPoolSettings.xml";
+      var appPool = FileSystem.Local.Zip.ZipContainsFile(filePath, AppPoolFileName);
+      if (!appPool)
+      {
+        WindowHelper.ShowMessage("Wrong package for import. The package does not contain the {0} file.".FormatWith(AppPoolFileName));
+        return;
+      }
+
+      const string WebsiteSettingsFileName = "WebsiteSettings.xml";
+      var websiteSettings = FileSystem.Local.Zip.ZipContainsFile(filePath, WebsiteSettingsFileName);
+      if (!websiteSettings)
+      {
+        WindowHelper.ShowMessage("Wrong package for import. The package does not contain the {0} file.".FormatWith(WebsiteSettingsFileName));
+
+        return;
+      }
+
+      const string WebConfigFileName = @"Website/Web.config";
+      if (!FileSystem.Local.Zip.ZipContainsFile(filePath, WebConfigFileName))
+      {
+        WindowHelper.ShowMessage("Wrong package for import. The package does not contain the {0} file.".FormatWith(WebConfigFileName));
+
+        return;
+      }
+
+      WizardPipelineManager.Start("import", mainWindow, null, null, MainWindowHelper.SoftlyRefreshInstances, filePath);
     }
 
     #endregion
