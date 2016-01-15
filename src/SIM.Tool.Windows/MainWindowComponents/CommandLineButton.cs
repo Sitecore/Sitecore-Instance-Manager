c33d6dec1020fe29cb2c9de158e29e8975d9b76e
AlenPelin <AlenPelin@sitecore.net>
2016-01-15 13:24:18 +1100

    SIM.exe was embedded to SIM.Tool (#19)

---
+++ src/SIM.Tool.Windows/MainWindowComponents/CommandLineButton.cs
@@ -0,0 +1,45 @@
+﻿namespace SIM.Tool.Windows.MainWindowComponents
+{
+  using System;
+  using System.Diagnostics;
+  using System.IO;
+  using System.Windows;
+  using SIM.Tool.Base;
+  using Sitecore.Diagnostics.Base.Annotations;
+
+  [UsedImplicitly]
+  public class CommandLineButton : WindowOnlyButton
+  {
+    protected override void OnClick([CanBeNull] Window mainWindow)
+    {
+      var appFilePath = ApplicationManager.GetEmbeddedFile("SIM.Tool.Windows", "SIM.exe");
+      var targetDir = Path.GetDirectoryName(appFilePath);
+      var sourceDir = Path.GetFullPath(".");
+      foreach (var sourceFilePath in Directory.GetFiles(sourceDir, "*", SearchOption.AllDirectories))
+      {
+        if (sourceFilePath.EndsWith("sim.exe", StringComparison.OrdinalIgnoreCase))
+        {
+          continue;
+        }
+
+        var targetFilePath = Path.Combine(targetDir, sourceFilePath.Substring(sourceDir.Length + 1));
+
+        var dir = Path.GetDirectoryName(targetFilePath);
+        if (!Directory.Exists(dir))
+        {
+          Directory.CreateDirectory(dir);
+        }
+
+        File.Copy(sourceFilePath, targetFilePath, true);
+      }
+
+      var start = new ProcessStartInfo("cmd.exe")
+      {
+        WorkingDirectory = targetDir,
+        UseShellExecute = true
+      };
+
+      WindowHelper.RunApp(start);
+    }
+  }
+}
