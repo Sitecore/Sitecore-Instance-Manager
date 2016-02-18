ae8ad35a44e77b207329502b81c267114aade1ae
AlenPelin <AlenPelin@gmail.com>
2016-02-18 11:32:52 +1100

    Refactoring.

--- src/SIM.Client/Commands/MainCommandGroup.cs
+++ src/SIM.Client/Commands/MainCommandGroup.cs
@@ -1,13 +1,9 @@
 ﻿namespace SIM.Client.Commands
 {
-  using System.Linq;
   using CommandLine;
-  using CommandLine.Text;
-  using SIM.Core.Common;
-  using Sitecore.Diagnostics.Base;
   using Sitecore.Diagnostics.Base.Annotations;
 
-  public class MainCommandGroup
+  public class MainCommandGroup : MainCommandGroupBase
   {
     #region Nested Commands
 
@@ -37,52 +33,5 @@
     public DeleteCommandFacade DeleteCommandFacade { get; set; }
 
     #endregion
-
-    [NotNull]
-    public ICommand SelectedCommand
-    {
-      get
-      {
-        var command = this.FindCommand(this);
-        Assert.IsNotNull(command, "There is no selected command");
-
-        return command;
-      }
-    }
-
-    [CanBeNull]
-    [UsedImplicitly]
-    [HelpVerbOption]
-    public string GetUsage([CanBeNull] string verb)
-    {
-      return HelpText.AutoBuild(this, verb);
-    }
-
-    [CanBeNull]
-    private ICommand FindCommand([NotNull] object commandContainer)
-    {
-      Assert.ArgumentNotNull(commandContainer, "commandContainer");
-
-      var properties = commandContainer.GetType().GetProperties();
-      foreach (var propertyInfo in properties)
-      {
-        if (propertyInfo == null || propertyInfo.GetCustomAttributes(true).ToArray().All(x => x == null || x is VerbOptionAttribute))
-        {
-          continue;
-        }
-
-        var innerCommand = propertyInfo.GetValue(commandContainer, null) as ICommand;
-        if (innerCommand != null)
-        {
-          var command = this.FindCommand(innerCommand);
-          if (command != null)
-          {
-            return command;
-          }
-        }
-      }
-
-      return commandContainer as ICommand;
-    }
   }
 }
---
+++ src/SIM.Client/Commands/MainCommandGroupBase.cs
@@ -0,0 +1,59 @@
+namespace SIM.Client.Commands
+{
+  using System.Linq;
+  using CommandLine;
+  using CommandLine.Text;
+  using Sitecore.Diagnostics.Base;
+  using Sitecore.Diagnostics.Base.Annotations;
+  using SIM.Core.Common;
+
+  public abstract class MainCommandGroupBase
+  {
+    [NotNull]
+    public ICommand SelectedCommand
+    {
+      get
+      {
+        var command = this.FindCommand(this);
+        Assert.IsNotNull(command, "There is no selected command");
+
+        return command;
+      }
+    }
+
+    [CanBeNull]
+    [UsedImplicitly]
+    [HelpVerbOption]
+    public string GetUsage([CanBeNull] string verb)
+    {
+      return HelpText.AutoBuild(this, verb);
+    }
+
+    [CanBeNull]
+    private ICommand FindCommand([NotNull] object commandContainer)
+    {
+      Assert.ArgumentNotNull(commandContainer, "commandContainer");
+
+      var properties = commandContainer.GetType().GetProperties();
+      foreach (var propertyInfo in properties)
+      {
+        if (propertyInfo == null || propertyInfo.GetCustomAttributes(true).ToArray().All(x => x == null || x is VerbOptionAttribute))
+        {
+          continue;
+        }
+
+        var innerCommand = propertyInfo.GetValue(commandContainer, null) as ICommand;
+        if (innerCommand != null)
+        {
+          var command = this.FindCommand(innerCommand);
+          if (command != null)
+          {
+            return command;
+          }
+        }
+      }
+
+      return commandContainer as ICommand;
+    }
+  }
+}
