3f93ce3755df2411f260a9e2569cd128c9b77974
AlenPelin <AlenPelin@gmail.com>
2016-05-23 11:06:53 +0300

    Fix command line help

--- src/SIM.Client/Program.cs
+++ src/SIM.Client/Program.cs
@@ -49,8 +49,9 @@
       if (!parser.ParseArguments(filteredArgs.ToArray(), options, delegate { }))
       {
         Console.WriteLine("Note, commands provide output when work is done i.e. without any progress indication.");
-        Console.WriteLine("\r\n  --query\t      When specified, allows returning only part of any command's output");
-        Console.WriteLine("\r\n  --wait\t       When specified, waits for keyboard input before terminating");
+        Console.WriteLine("\r\n  --query\t   When specified, allows returning only part of any command's output");
+        Console.WriteLine("\r\n  --wait\t   When specified, waits for keyboard input before terminating");
+
         Environment.Exit(Parser.DefaultExitCodeFail);
       }
 
