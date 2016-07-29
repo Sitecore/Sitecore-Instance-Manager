5d4e49bbb3639cdecda3b6a1870d2886984f5b68
AlenPelin <AlenPelin@gmail.com>
2016-07-29 11:55:42 +1000

    Refactor NuGet generator button

--- src/SIM.Tool.Windows/MainWindowComponents/GenerateNuGetPackagesButton.cs
+++ src/SIM.Tool.Windows/MainWindowComponents/GenerateNuGetPackagesButton.cs
@@ -2,17 +2,12 @@
 {
   using System;
   using System.IO;
-  using System.Linq;
   using System.Windows;
-  using System.Xml;
   using SIM.Instances;
   using SIM.Tool.Base;
   using SIM.Tool.Base.Plugins;
   using Sitecore.Diagnostics.Base;
   using Sitecore.Diagnostics.Base.Annotations;
-  using Sitecore.Diagnostics.Logging;
-  using Sitecore.NuGet.Core;
-  using SIM.Extensions;
   using SIM.Tool.Base.Profiles;
 
   [UsedImplicitly]
@@ -29,7 +24,7 @@
     {
       Assert.ArgumentNotNull(mainWindow, nameof(mainWindow));
 
-      var nugetFolderPath = Environment.ExpandEnvironmentVariables(WindowsSettings.AppNuGetDirectory.Value);
+      var nugetFolderPath = NuGetHelper.NuGetFolderPath;
 
       Action longRunningTask = delegate
       {
@@ -38,71 +33,13 @@
           Directory.CreateDirectory(nugetFolderPath);
         }
 
-        UpdateSettings(nugetFolderPath);
-        GeneratePackages(nugetFolderPath);
+        NuGetHelper.UpdateSettings(nugetFolderPath);
+        NuGetHelper.GeneratePackages(ProfileManager.Profile.LocalRepository, nugetFolderPath);
       };
 
       var content = $"The SC.* NuGet packages are now being generated in the {nugetFolderPath} directory for all Sitecore versions that exist in the local repository. Read more: {Link}";
 
       WindowHelper.LongRunningTask(longRunningTask, "Generating NuGet Packages", mainWindow, null, content, true);
-    }
-
-    private static void GeneratePackages([NotNull] string nugetFolderPath)
-    {
-      Assert.ArgumentNotNull(nugetFolderPath, nameof(nugetFolderPath));
-
-      Log.Info("Generating NuGet packages from {0} to {1}", ProfileManager.Profile.LocalRepository, nugetFolderPath);
-      ProcessFolder(ProfileManager.Profile.LocalRepository, nugetFolderPath);
-    }
-
-    private static void ProcessFolder(string directory, string outputFolderPath)
-    {
-      var pattern = "*rev*.zip";
-      var zipfiles = Directory.GetFiles(directory, pattern, SearchOption.AllDirectories);
-      foreach (var file in zipfiles)
-      {
-        try
-        {
-          Log.Info("Generating NuGet packages from: " + file);
-
-          // Create nupkg file
-          new PackageGenerator().Generate(file, outputFolderPath);
-        }
-        catch (Exception ex)
-        {
-          Log.Error("Error processing file " + file + ". " + ex.Message + Environment.NewLine + "Stack trace:" + Environment.NewLine + ex.StackTrace);
-        }
-      }
-    }
-
-    private static void UpdateSettings([NotNull] string nugetFolderPath)
-    {
-      Assert.ArgumentNotNull(nugetFolderPath, nameof(nugetFolderPath));
-
-      var nugetConfigPath = Environment.ExpandEnvironmentVariables(@"%appdata%\NuGet\nuget.config");
-      var bakFilePath = nugetConfigPath + ".bak";
-      if (!File.Exists(bakFilePath))
-      {
-        File.Copy(nugetConfigPath, bakFilePath);
-      }
-
-      var nugetConfig = XmlDocumentEx.LoadFile(nugetConfigPath);
-      Assert.IsNotNull(nugetConfig, nameof(nugetConfig));
-
-      var config = nugetConfig.SelectSingleNode("/configuration") ?? nugetConfig.DocumentElement.AddElement("configuration");
-      Assert.IsNotNull(config, nameof(config));
-
-      var keyName = "Sitecore NuGet";
-      var packageSources = nugetConfig.SelectSingleElement("/configuration/packageSources") ?? config.AddElement("packageSources");
-      Assert.IsNotNull(packageSources, nameof(packageSources));
-
-      var addElements = packageSources.ChildNodes.OfType<XmlElement>();
-      var add = addElements.FirstOrDefault(x => x.GetAttribute("key").Equals(keyName, StringComparison.OrdinalIgnoreCase)) ?? packageSources.AddElement("add");
-      Assert.IsNotNull(add, nameof(add));
-
-      add.SetAttribute("key", keyName);
-      add.SetAttribute("value", nugetFolderPath);
-      nugetConfig.Save();
     }
   }
 }
---
+++ src/SIM.Tool.Windows/NuGetHelper.cs
@@ -0,0 +1,82 @@
+﻿namespace SIM.Tool.Windows
+{
+  using System;
+  using System.IO;
+  using System.Linq;
+  using System.Xml;
+  using Sitecore.Diagnostics.Base;
+  using Sitecore.Diagnostics.Base.Annotations;
+  using Sitecore.Diagnostics.Logging;
+  using Sitecore.NuGet.Core;
+  using SIM.Extensions;
+  using SIM.Tool.Base.Profiles;
+
+  public static class NuGetHelper
+  {
+    public static string NuGetFolderPath => Environment.ExpandEnvironmentVariables(WindowsSettings.AppNuGetDirectory.Value);
+
+    public static void GeneratePackages([NotNull] string directory, [CanBeNull] string outputFolderPath)
+    {
+      Assert.ArgumentNotNull(directory, nameof(directory));
+
+      outputFolderPath = outputFolderPath ?? NuGetFolderPath;
+
+      Log.Info("Generating NuGet packages from {0} to {1}", ProfileManager.Profile.LocalRepository, outputFolderPath);
+
+      var pattern = "*rev*.zip";
+      var zipfiles = Directory.GetFiles(directory, pattern, SearchOption.AllDirectories);
+      foreach (var filePath in zipfiles)
+      {
+        var file = new FileInfo(filePath);
+        try
+        {
+          GeneratePackages(file, outputFolderPath);
+        }
+        catch (Exception ex)
+        {
+          Log.Error("Error processing file " + file + ". " + ex.Message + Environment.NewLine + "Stack trace:" + Environment.NewLine + ex.StackTrace);
+        }
+      }
+    }
+
+    public static void GeneratePackages([NotNull] FileInfo file, [CanBeNull] string outputFolderPath = null)
+    {
+      Assert.ArgumentNotNull(file, nameof(file));
+
+      Log.Info("Generating NuGet packages from {0} to {1} ", file, outputFolderPath);
+
+      // Create nupkg file
+      new PackageGenerator().Generate(file.FullName, outputFolderPath ?? NuGetFolderPath);
+    }
+
+    public static void UpdateSettings([CanBeNull] string nugetFolderPath = null)
+    {
+      nugetFolderPath = nugetFolderPath ?? NuGetFolderPath;
+
+      var nugetConfigPath = Environment.ExpandEnvironmentVariables(@"%appdata%\NuGet\nuget.config");
+      var bakFilePath = nugetConfigPath + ".bak";
+      if (!File.Exists(bakFilePath))
+      {
+        File.Copy(nugetConfigPath, bakFilePath);
+      }
+
+      var nugetConfig = XmlDocumentEx.LoadFile(nugetConfigPath);
+      Assert.IsNotNull(nugetConfig, nameof(nugetConfig));
+
+      var config = nugetConfig.SelectSingleNode("/configuration") ?? nugetConfig.DocumentElement.AddElement("configuration");
+      Assert.IsNotNull(config, nameof(config));
+
+      var keyName = "Sitecore NuGet";
+      var packageSources = nugetConfig.SelectSingleElement("/configuration/packageSources") ?? config.AddElement("packageSources");
+      Assert.IsNotNull(packageSources, nameof(packageSources));
+
+      var addElements = packageSources.ChildNodes.OfType<XmlElement>();
+      var add = addElements.FirstOrDefault(x => x.GetAttribute("key").Equals(keyName, StringComparison.OrdinalIgnoreCase)) ?? packageSources.AddElement("add");
+      Assert.IsNotNull(add, nameof(add));
+
+      add.SetAttribute("key", keyName);
+      add.SetAttribute("value", nugetFolderPath);
+      nugetConfig.Save();
+    }
+  }
+}
