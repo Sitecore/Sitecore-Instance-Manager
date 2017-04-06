2376a2e2dcf97879dfb9a646267ff1fa497418d0
AlenPelin <AlenPelin@gmail.com>
2017-04-07 09:54:20 +1000

    Refactor sync command and fix issues

--- src/SIM.Core/Commands/SyncCommand.cs
+++ src/SIM.Core/Commands/SyncCommand.cs
@@ -18,6 +18,15 @@
     [CanBeNull]
     public virtual string Ignore { get; [UsedImplicitly] set; }
 
+    [NotNull]
+    private readonly HandlerBase OnChanged = new ChangedHandler();
+
+    [NotNull]
+    private readonly HandlerBase OnDeleted = new DeletedHandler();
+
+    [NotNull]
+    private readonly HandlerBase OnRenamed = new RenamedHandler();
+
     protected override void DoExecute([NotNull] IReadOnlyList<Instance> instances, CommandResult result)
     {
       Assert.ArgumentNotNull(instances, nameof(instances));
@@ -29,26 +38,33 @@
         .ToArray();
 
       var websiteFolders = instances.Select(x => x.WebRootPath).ToArray();
+      var watchers = new List<FileSystemWatcher>();
       foreach (var websiteFolder in websiteFolders)
       {
         var watcher = new FileSystemWatcher
         {
           Path = websiteFolder,
           Filter = "*.*",
-          EnableRaisingEvents = true,
-          NotifyFilter = NotifyFilters.LastWrite,
-          IncludeSubdirectories = true
+          IncludeSubdirectories = true,
+          InternalBufferSize = int.MaxValue / 2
         };
+
+        watcher.Error += (sender, args) => Console.WriteLine($"{DateTime.Now:HH:mm:ss} ERRROR: {args.GetException()?.Message}");
 
         watcher.Created += (sender, args) => Update(args, websiteFolder, targetRoots, ignore);
         watcher.Changed += (sender, args) => Update(args, websiteFolder, targetRoots, ignore);
+        watcher.Renamed += (sender, args) => Update(args, websiteFolder, targetRoots, ignore);
         watcher.Deleted += (sender, args) => Update(args, websiteFolder, targetRoots, ignore);
+
+        watcher.EnableRaisingEvents = true;
       }
 
       Console.WriteLine("Waiting for file system changes...");
       Console.WriteLine();
       Console.WriteLine("[Press any key to abort]");
       Console.ReadKey();
+      
+      watchers.ForEach(x => x?.Dispose());
     }
 
     private void Update([CanBeNull] FileSystemEventArgs args, [NotNull] string sourceRoot, [NotNull] string[] targetRoots, [NotNull] string[] ignore)
@@ -63,6 +79,13 @@
       if (sourcePath.Contains("\\.git\\") || sourcePath.EndsWith("\\.git"))
       {
         return;
+      }                        
+
+      var fileName = Path.GetFileName(sourcePath);
+      if (!fileName.Contains('.'))
+      {
+        // do not support files without dot
+        return;
       }
 
       if (ignore.Any(x => sourcePath.IndexOf(x, StringComparison.OrdinalIgnoreCase) >= 0))
@@ -70,69 +93,209 @@
         return;
       }
 
-      if (!File.Exists(sourcePath))
+      switch (args.ChangeType)
       {
-        return;
+        case WatcherChangeTypes.Deleted:
+          OnDeleted.Process(args, sourceRoot, targetRoots);
+          return;
+
+        case WatcherChangeTypes.Renamed:
+          OnRenamed.Process(args, sourceRoot, targetRoots);
+          return;
+
+        default:
+          OnChanged.Process(args, sourceRoot, targetRoots);
+          return;
+      }      
+    }
+
+    private class ChangedHandler : HandlerBase
+    {
+      protected override bool PreProcess(FileSystemEventArgs args)
+      {
+        Assert.ArgumentNotNull(args, nameof(args));
+
+        var sourcePath = args.FullPath;
+        Assert.IsNotNull(sourcePath, nameof(sourcePath));         
+
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss} CHANGED: {sourcePath}");
+
+        return true;
       }
 
-      var isDeleted = args.ChangeType == WatcherChangeTypes.Deleted;
-      Console.WriteLine(isDeleted ? $"{DateTime.Now:HH:mm:ss} DELETED: {sourcePath}" : $"{DateTime.Now:HH:mm:ss} CHANGED: {sourcePath}");
-
-      var retryList = new List<string>();
-      var virtualPath = sourcePath.Substring((sourceRoot.TrimEnd('\\') + "\\").Length);
-      foreach (var targetRoot in targetRoots)
+      protected override void TryProcess(FileSystemEventArgs args, string sourceRoot, string targetRoot)
       {
-        var targetPath = Path.Combine(targetRoot, virtualPath);
-        if (Path.GetFullPath(targetRoot).Equals(sourcePath))
-        {
-          // this can be when syncing A;B => B;C
-          continue;
-        }
+        var relativePath = args.Name;
+        Assert.IsNotNull(relativePath, nameof(relativePath));
 
-        try
-        {
-          File.Copy(sourcePath, targetPath, true);
-          Console.WriteLine($"{DateTime.Now:HH:mm:ss} +COPIED: {targetPath}");
-        }
-        catch
-        {
-          retryList.Add(targetPath);
-          Console.WriteLine($"{DateTime.Now:HH:mm:ss} ?FAILED: {targetPath}");
-        }
+        var targetPath = Path.Combine(targetRoot, relativePath);
+        Assert.IsNotNull(relativePath, nameof(relativePath));
+
+        var targetDir = Path.GetDirectoryName(targetPath);
+        Directory.CreateDirectory(targetDir);
+
+        var sourcePath = args.FullPath;
+        Assert.IsNotNullOrEmpty(sourcePath, nameof(sourcePath));
+
+        File.Copy(sourcePath, targetPath, true);
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss} +COPIED: {targetPath}");
+      }
+    }
+
+    private class RenamedHandler : HandlerBase
+    {
+      protected override bool PreProcess(FileSystemEventArgs args)
+      {
+        Assert.ArgumentNotNull(args, nameof(args));
+
+        var sourcePath = args.FullPath;
+        Assert.IsNotNull(sourcePath, nameof(sourcePath));        
+
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss} RENAMED: {sourcePath}");
+
+        return true;
       }
 
-      for (var i = 0; i < 10 && retryList.Any(); ++i)
+      protected override void TryProcess(FileSystemEventArgs args, string sourceRoot, string targetRoot)
       {
-        Thread.Sleep(1000);
-        var targetPaths = retryList.ToArray();
-        foreach (var targetPath in targetPaths)
+        Assert.ArgumentNotNull(args, nameof(args));
+        Assert.ArgumentNotNullOrEmpty(sourceRoot, nameof(sourceRoot));
+        Assert.ArgumentNotNullOrEmpty(targetRoot, nameof(targetRoot));
+
+        var relatviePath = args.Name;
+        Assert.IsNotNull(relatviePath, nameof(relatviePath));
+
+        var targetPath = Path.Combine(targetRoot, relatviePath);
+        Assert.IsNotNull(targetPath, nameof(targetPath));
+
+        var targetDir = Path.GetDirectoryName(targetPath);
+        Directory.CreateDirectory(targetDir);
+
+        var rArgs = (RenamedEventArgs)args;
+
+        var oldRelativePath = rArgs.OldName;
+        Assert.IsNotNull(oldRelativePath, nameof(oldRelativePath));
+
+        var oldTargetPath = Path.Combine(targetRoot, oldRelativePath);
+
+        // delete old file in targetRoot
+        if (File.Exists(oldTargetPath))
         {
+          File.Delete(oldTargetPath);
+          Console.WriteLine($"{DateTime.Now:HH:mm:ss} -KILLED: {oldTargetPath}");
+        }
+
+        // copy from sourcePath to targetPath
+        File.Copy(args.FullPath, targetPath);
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss} +COPIED: {targetPath}");
+      }
+    }
+
+    private class DeletedHandler : HandlerBase
+    {
+      protected override bool PreProcess(FileSystemEventArgs args)
+      {
+        Assert.ArgumentNotNull(args, nameof(args));
+
+        var sourcePath = args.FullPath;
+        Assert.IsNotNull(sourcePath, nameof(sourcePath));
+
+        // file exists, but must be absent
+        if (File.Exists(sourcePath))
+        {
+          return false;
+        }
+
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss} DELETED: {sourcePath}");
+
+        return true;
+      }
+
+      protected override void TryProcess(FileSystemEventArgs args, string sourceRoot, string targetRoot)
+      {
+        Assert.ArgumentNotNull(args, nameof(args));
+        Assert.ArgumentNotNullOrEmpty(sourceRoot, nameof(sourceRoot));
+        Assert.ArgumentNotNullOrEmpty(targetRoot, nameof(targetRoot));
+
+        var relatviePath = args.Name;
+        Assert.IsNotNull(relatviePath, nameof(relatviePath));
+
+        var targetPath = Path.Combine(targetRoot, relatviePath);
+        Assert.IsNotNull(targetPath, nameof(targetPath));
+
+        File.Delete(targetPath);
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss} -KILLED: {targetPath}");
+      }
+    }
+
+    private abstract class HandlerBase
+    {     
+      protected abstract bool PreProcess([NotNull] FileSystemEventArgs args);
+
+      protected abstract void TryProcess([NotNull] FileSystemEventArgs args, [NotNull] string sourceRoot, [NotNull] string targetPath);
+
+      public void Process([NotNull] FileSystemEventArgs args, [NotNull] string sourceRoot, [NotNull] string[] targetRoots)
+      {
+        Assert.ArgumentNotNull(args, nameof(args));
+        Assert.ArgumentNotNullOrEmpty(sourceRoot, nameof(sourceRoot));
+        Assert.ArgumentNotNull(targetRoots, nameof(targetRoots));
+
+        if (!PreProcess(args))
+        {
+          return;
+        }
+          
+        var relativePath = args.Name;
+        Assert.IsNotNull(relativePath, nameof(relativePath));
+
+        var retryList = new List<string>();
+
+        foreach (var targetRoot in targetRoots)
+        {
+          Assert.IsNotNullOrEmpty(targetRoot, nameof(targetRoot));
+
+          if (sourceRoot.Equals(targetRoot, StringComparison.OrdinalIgnoreCase))
+          {
+            // skip B => B when A, B => B, C
+            continue;
+          }
+
           try
           {
-            if (isDeleted)
-            {
-              File.Delete(targetPath);
-              Console.WriteLine($"{DateTime.Now:HH:mm:ss} -KILLED: {targetPath}");
-            }
-            else
-            {
-              File.Copy(sourcePath, targetPath, true);
-              Console.WriteLine($"{DateTime.Now:HH:mm:ss} +COPIED: {targetPath}");
-            }
-            retryList.Remove(targetPath);
+            TryProcess(args, sourceRoot, targetRoot);
           }
           catch
           {
+            retryList.Add(targetRoot);
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss} ?FAILED: {targetRoot}\\{relativePath}");
+          }
+        }       
+
+        for (var i = 0; i < 10 && retryList.Any(); ++i)
+        {
+          Thread.Sleep(1000);
+
+          var retryTargetRoots = retryList.ToArray();
+          foreach (var targetRoot in retryTargetRoots)
+          {
+            try
+            {
+              TryProcess(args, sourceRoot, targetRoot);
+              retryList.Remove(targetRoot);
+            }
+            catch
+            {
+            }
           }
         }
-      }
 
-      foreach (var targetPath in retryList)
-      {
-        Console.WriteLine($"{DateTime.Now:HH:mm:ss} ! FATAL: {targetPath}");
-      }
+        foreach (var targetRoot in retryList)
+        {
+          Console.WriteLine($"{DateTime.Now:HH:mm:ss} ! FATAL: {targetRoot}\\{relativePath}");
+        }
 
-      Console.WriteLine();
+        Console.WriteLine();
+      }
     }
   }
 }
