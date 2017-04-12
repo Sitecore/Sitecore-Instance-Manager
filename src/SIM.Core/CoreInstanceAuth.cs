8917f513cbeea6f9c0ec46ea7ecd0cbdf1be11f2
AlenPelin <AlenPelin@gmail.com>
2017-04-12 16:25:51 +1000

    Fix broken bypass security button

--- src/SIM.Core/CoreInstanceAuth.cs
+++ src/SIM.Core/CoreInstanceAuth.cs
@@ -45,8 +45,8 @@
     private const string FileContentsPattern = @"<%@ Page Language=""C#"" %>
 
 <script runat=""server"">
-  private static object SyncRoot { get; } = new object();
-  private static DateTime endDate { get; } = DateTime.Parse(""DATETIME_NOW"", System.Globalization.CultureInfo.InvariantCulture);
+  private static readonly object SyncRoot = new object();
+  private static readonly DateTime EndDate = DateTime.Parse(""DATETIME_NOW"", System.Globalization.CultureInfo.InvariantCulture);
   private static bool done;
   void Page_Load(object sender, EventArgs e)
   {
@@ -54,7 +54,7 @@
     {
       try
       {
-        if (!done && DateTime.Now <= endDate)
+        if (!done && DateTime.Now <= EndDate)
         {
           var shellUrlPrefix = @""/sitecore/shell"";
           var userName = Request.QueryString[""user""] ?? ""sitecore\\admin"";
