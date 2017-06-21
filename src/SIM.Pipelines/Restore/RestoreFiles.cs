686b0ebdd36cb2a09ac524498386eb8580f5ecef
peterpociask <github@pociask.com>
2017-06-21 16:53:41 -0400

    Adding code to delete folder content before restoring from zip. Solves issue 159.

--- src/SIM.Pipelines/Restore/RestoreFiles.cs
+++ src/SIM.Pipelines/Restore/RestoreFiles.cs
@@ -1,58 +1,63 @@
 ﻿namespace SIM.Pipelines.Restore
 {
-  using Sitecore.Diagnostics.Base;
-  using JetBrains.Annotations;
+    using Sitecore.Diagnostics.Base;
+    using JetBrains.Annotations;
 
-  #region
-
-  #endregion
-
-  [UsedImplicitly]
-  public class RestoreFiles : RestoreProcessor
-  {
-    #region Constants
-
-    private const int A = 40;
-
-    private const int B = 5;
+    #region
 
     #endregion
 
-    #region Methods
-
-    protected override long EvaluateStepsCount(RestoreArgs args)
+    [UsedImplicitly]
+    public class RestoreFiles : RestoreProcessor
     {
-      Assert.ArgumentNotNull(args, nameof(args));
+        #region Constants
 
-      return args.Backup.BackupDataFiles ? A + B : A;
-    }
+        private const int A = 40;
 
-    protected override bool IsRequireProcessing(RestoreArgs args)
-    {
-      Assert.ArgumentNotNull(args, nameof(args));
+        private const int B = 5;
 
-      return args.Backup.BackupWebsiteFiles;
-    }
+        #endregion
 
-    protected override void Process([NotNull] RestoreArgs args)
-    {
-      Assert.ArgumentNotNull(args, nameof(args));
+        #region Methods
 
-      var webRootPath = args._WebRootPath;
-      if (args.Backup.BackupWebsiteFiles)
-      {
-        FileSystem.FileSystem.Local.Zip.UnpackZip(
-          args.Backup.BackupWebsiteFilesNoClient ? args.Backup.WebRootNoClientFilePath : args.Backup.WebRootFilePath, 
+        protected override long EvaluateStepsCount(RestoreArgs args)
+        {
+            Assert.ArgumentNotNull(args, nameof(args));
+
+            return args.Backup.BackupDataFiles ? A + B : A;
+        }
+
+        protected override bool IsRequireProcessing(RestoreArgs args)
+        {
+            Assert.ArgumentNotNull(args, nameof(args));
+
+            return args.Backup.BackupWebsiteFiles;
+        }
+
+        protected override void Process([NotNull] RestoreArgs args)
+        {
+            Assert.ArgumentNotNull(args, nameof(args));
+
+            var webRootPath = args._WebRootPath;
+            if (args.Backup.BackupWebsiteFiles)
+            {
+                //Remove folder content before unzipping the backup into the same location. 
+                //Potentially dangerous operation. This should probably be triggered by a flag. 
+                var directoryProvider = new FileSystem.DirectoryProvider(FileSystem.FileSystem.Local);
+                directoryProvider.Delete(webRootPath);
+
+                FileSystem.FileSystem.Local.Zip.UnpackZip(
+          args.Backup.BackupWebsiteFilesNoClient ? args.Backup.WebRootNoClientFilePath : args.Backup.WebRootFilePath,
           webRootPath, null, A);
-      }
+            }
 
-      if (args.Backup.BackupDataFiles)
-      {
-        IncrementProgress();
-        FileSystem.FileSystem.Local.Zip.UnpackZip(args.Backup.DataFilePath, args._DataFolder, null, B);
-      }
+            if (args.Backup.BackupDataFiles)
+            {
+                IncrementProgress();
+                FileSystem.FileSystem.Local.Zip.UnpackZip(args.Backup.DataFilePath, args._DataFolder, null, B);
+            }
+        }
+
+        #endregion
     }
-
-    #endregion
-  }
 }
