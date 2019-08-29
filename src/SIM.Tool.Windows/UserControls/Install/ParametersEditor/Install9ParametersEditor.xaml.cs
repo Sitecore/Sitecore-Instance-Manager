700044a623726e18bbb768d4047cce9faa93e7fb
Ivan Sheyenko <ivs@sitecore.net>
2019-08-29 13:35:10 -0400

    do not show tabs for tasks without parameters

--- src/SIM.Tool.Windows/UserControls/Install/ParametersEditor/Install9ParametersEditor.xaml.cs
+++ src/SIM.Tool.Windows/UserControls/Install/ParametersEditor/Install9ParametersEditor.xaml.cs
@@ -30,7 +30,7 @@ namespace SIM.Tool.Windows.UserControls.Install.ParametersEditor
       var tasker = this.DataContext as Tasker;
       List<TasksModel> model = new List<TasksModel>();
       model.Add(new TasksModel("Global", tasker.GlobalParams));
-      foreach (PowerShellTask task in tasker.Tasks.Where(t=>t.ShouldRun))
+      foreach (PowerShellTask task in tasker.Tasks.Where(t=>t.ShouldRun&&t.LocalParams.Any()))
       {
         if (!tasker.UnInstall || (tasker.UnInstall && task.SupportsUninstall()))
         {
