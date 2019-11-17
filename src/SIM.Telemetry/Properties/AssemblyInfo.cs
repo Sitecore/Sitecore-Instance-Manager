e8b3e776b7fb1ce39add2ae6e93821fcfd79bf32
Andrey Filchenkov <22079019+AndreyFilchenkov@users.noreply.github.com>
2019-11-17 12:10:30 +0200

    Add AssemblyDescription to SIM.Telemetry project

--- src/SIM.Telemetry/Properties/AssemblyInfo.cs
+++ src/SIM.Telemetry/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SIM.Telemetry")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("'SIM.Telemetry' is used to track Sitecore Instance Manager utilisation statistics")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SIM.Telemetry")]
