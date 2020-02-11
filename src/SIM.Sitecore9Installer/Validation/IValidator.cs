5e7c42f101968781263f458825a467420f3b20fd
Ivan Sheyenko <ivs@sitecore.net>
2020-02-11 15:38:08 -0500

    Feature/365 validation engine (#369)
    * remove old validation classes, as not used. #365
    
    * Validation factory and absctractions. #365
    
    * Change separator to '|'. #365
    
    * Validation framework. #365
    
    * parameters evaluation in parallel. #365
    
    * Engine optimization. #365
    
    * error handling. #365
    
    * Make ValidationFactory singleton. #365
    
    * Unit tests. #365
    
    * make sure that Messages property is assigned. #365
    
    * Make validation optional. #365
    
    * Introduce Data property for validators to pass additional parameters to validators. #365
    
    * Refactor unit tests. #365
    
    * Do not overwrite solr service with empty value. #365
    
    * Remove var, move intialization to constructor. #365

---
+++ src/SIM.Sitecore9Installer/Validation/IValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SIM.Sitecore9Installer.Tasks;
+
+namespace SIM.Sitecore9Installer.Validation
+{
+  public interface IValidator
+  {
+    IEnumerable<ValidationResult> Evaluate(IEnumerable<Task> tasks);
+    Dictionary<string,string> Data { get; set; }
+  }
+}
---
+++ src/SIM.Sitecore9Installer/Validation/ValidationFactory.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SIM.Sitecore9Installer.Validation
+{
+  public class ValidationFactory
+  {
+    private  Dictionary<string, IValidator> validators;
+    private  Dictionary<string, List<string>> validatorLists;
+    private static ValidationFactory _instance;
+
+    internal ValidationFactory()
+    {
+      this.validators = new Dictionary<string, IValidator>();
+      JObject doc =
+        JObject.Parse(File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(),
+          "GlobalParamsConfig/Validators.json")));
+      List<ValidatorDefinition> validatorDefinitions =
+        doc["ValidatorDefinitions"].ToObject<List<ValidatorDefinition>>();
+      foreach (ValidatorDefinition definition in validatorDefinitions)
+      {
+        IValidator validator = (IValidator)Activator.CreateInstance(Type.GetType(definition.Type));
+        validator.Data = definition.Data;
+        validators.Add(definition.Name, validator);
+      }
+
+      validatorLists = doc["ValidatorLists"].ToObject<Dictionary<string, List<string>>>();
+    }
+
+    public static ValidationFactory Instance
+    {
+      get
+      {
+        if (_instance == null)
+        {
+          _instance = new ValidationFactory();
+        }
+
+        return _instance;
+      }
+    }
+
+
+    public  IEnumerable<IValidator> GetValidators(IEnumerable<string> names)
+    {
+      List<IValidator> result = new List<IValidator>();
+      foreach (string name in names)
+      {
+        if (name.StartsWith("list|"))
+        {
+          string listName = name.Split('|')[1];
+          if (validatorLists.ContainsKey(listName))
+          {
+            foreach (string validatorName in validatorLists[listName])
+            {
+              if (validators.ContainsKey(validatorName))
+              {
+                result.Add(validators[validatorName]);
+              }
+            }
+          }
+        }
+        else
+        {
+          if (validators.ContainsKey(name))
+          {
+            result.Add(validators[name]);
+          }
+        }
+      }
+
+      return result;
+    }
+  }
+}
