6fed595093fc97b0e691aa5b80a370aec959d17a
Ivan Sheyenko <ivs@sitecore.net>
2020-02-11 15:31:52 -0500

    Remove var, move intialization to constructor. #365

--- src/SIM.Sitecore9Installer/Validation/ValidationFactory.cs
+++ src/SIM.Sitecore9Installer/Validation/ValidationFactory.cs
@@ -1,27 +1,25 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
 
 namespace SIM.Sitecore9Installer.Validation
 {
   public class ValidationFactory
   {
-    private  Dictionary<string, IValidator> validators=new Dictionary<string, IValidator>();
+    private  Dictionary<string, IValidator> validators;
     private  Dictionary<string, List<string>> validatorLists;
     private static ValidationFactory _instance;
 
     internal ValidationFactory()
     {
+      this.validators = new Dictionary<string, IValidator>();
       JObject doc =
         JObject.Parse(File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(),
           "GlobalParamsConfig/Validators.json")));
       List<ValidatorDefinition> validatorDefinitions =
         doc["ValidatorDefinitions"].ToObject<List<ValidatorDefinition>>();
-      foreach (var definition in validatorDefinitions)
+      foreach (ValidatorDefinition definition in validatorDefinitions)
       {
         IValidator validator = (IValidator)Activator.CreateInstance(Type.GetType(definition.Type));
         validator.Data = definition.Data;
