fc1c379fcd4af974405916ac3e5a0d6f9715ff95
Ivan Sheyenko <ivs@sitecore.net>
2020-02-12 16:09:35 -0500

    Solr service validation. #356

---
+++ src/SIM.Sitecore9Installer.Tests/Validation/Validators/SolrServiceValidatorTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using NSubstitute;
+using SIM.Sitecore9Installer.Tasks;
+using SIM.Sitecore9Installer.Validation;
+using SIM.Sitecore9Installer.Validation.Validators;
+using Xunit;
+
+namespace SIM.Sitecore9Installer.Tests.Validation.Validators
+{
+  public class SolrServiceValidatorTests
+  {
+    [Theory]
+    [ClassData(typeof(ValidatorTestSetup))]
+    public void ServiceDoesNotExist(IEnumerable<Task> tasks)
+    {
+      foreach (Task t in tasks)
+      {
+        t.LocalParams.Add(new InstallParam("SolrService", "nope"));
+      }
+
+      SolrServiceValidator val = Substitute.ForPartsOf<SolrServiceValidator>();
+      val.Data["ParamNames"] = Guid.NewGuid().ToString();
+      ServiceControllerWrapper s = null;
+      val.GetService(Arg.Any<string>()).ReturnsForAnyArgs(s);
+      IEnumerable<ValidationResult> res = val.Evaluate(tasks);
+      Assert.Equal(0, res.Count(r => r.State == ValidatorState.Error));
+    }
+
+    [Theory]
+    [ClassData(typeof(ValidatorTestSetup))]
+    public void ServiceIsRunning(IEnumerable<Task> tasks)
+    {
+      foreach (Task t in tasks)
+      {
+        t.LocalParams.Add(new InstallParam("SolrService", "nope"));
+      }
+
+      SolrServiceValidator val = Substitute.ForPartsOf<SolrServiceValidator>();
+      val.Data["ParamNames"] = Guid.NewGuid().ToString();
+      ServiceController c = null;
+      ServiceControllerWrapper s = Substitute.For<ServiceControllerWrapper>(c);
+      s.Status.Returns(ServiceControllerStatus.Running);
+      val.GetService(Arg.Any<string>()).ReturnsForAnyArgs(s);
+      IEnumerable<ValidationResult> res = val.Evaluate(tasks);
+      Assert.Equal(0, res.Count(r => r.State == ValidatorState.Error));
+    }
+  }
+}
