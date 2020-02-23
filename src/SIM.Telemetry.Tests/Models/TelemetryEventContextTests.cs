18b16d85d6274558807d8bb9b225b76a716d897f
Andrey Filchenkov <22079019+AndreyFilchenkov@users.noreply.github.com>
2020-02-24 01:00:20 +0200

    Add Telemetry.Tests project. Add nuspec file. Telemetry event can have customEventData.

---
+++ src/SIM.Telemetry.Tests/Models/TelemetryEventContextTests.cs
@@ -0,0 +1,41 @@
+﻿using SIM.Telemetry.Models;
+using System;
+using Xunit;
+using Sitecore.Diagnostics.Base.Exceptions;
+
+namespace SIM.Telemetry.Tests.Models
+{
+  public class TelemetryEventContextTests
+  {
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void Ctor_Should_ThrowArgumentNull(string version)
+    {
+      // Arrange
+      var app = Guid.NewGuid();
+      var device = Guid.NewGuid();
+
+      // Act
+      Action action = () => new TelemetryEventContext(app, device, version);
+
+      // Assert
+      Assert.Throws<ArgumentNullOrEmptyException>(action);
+    }
+
+    [Theory]
+    [InlineData("0.0.0.0")]
+    public void Ctor_ShouldNotThrow_ArgumentNullOrEmpty(string version)
+    {
+      // Arrange
+      var app = Guid.NewGuid();
+      var device = Guid.NewGuid();
+
+      // Act
+      var context = new TelemetryEventContext(app, device, version);
+
+      // Assert
+      Assert.NotNull(context);
+    }
+  }
+}
