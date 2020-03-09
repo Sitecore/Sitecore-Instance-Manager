6eb0daaedb61d0862249b27df484e9455699e4f0
Andrey Filchenkov <22079019+AndreyFilchenkov@users.noreply.github.com>
2020-03-09 20:51:13 +0200

    Add 'NugetToolPresenceValidator' (closes #352)

---
+++ src/SIM.Sitecore9Installer.Tests/Validation/Validators/NugetToolPresenceValidatorTests.cs
@@ -0,0 +1,76 @@
+﻿using NSubstitute;
+using SIM.Sitecore9Installer.Validation.Validators;
+using System.Linq;
+using Xunit;
+
+namespace SIM.Sitecore9Installer.Tests.Validation.Validators
+{
+  public class NugetToolPresenceValidatorTests : NugetToolPresenceValidator
+  {
+    [Theory]
+    [InlineData("somenonexistingcommand-9583457661d0")]
+    public void GetScriptError_Should_ReturnError(string command)
+    {
+      // PS script should throw the "CommandNotFoundException" error since we are running non-existing command.
+
+      // Arrange
+      string script = command;
+      NugetToolPresenceValidatorTests validator = new NugetToolPresenceValidatorTests();
+
+      // Act
+      string result = validator.GetScriptError(script);
+
+      // Assert
+      Assert.Equal("CommandNotFoundException", result);
+    }
+
+    [Theory]
+    [InlineData("Get-Location")]
+    public void GetScriptError_ShouldNot_ReturnError(string command)
+    {
+      // PS scripts should not throw any error since we are running well known('Get-Location') command.
+
+      // Arrange
+      string script = command;
+      NugetToolPresenceValidatorTests validator = new NugetToolPresenceValidatorTests();
+
+      // Act
+      string result = validator.GetScriptError(script);
+
+      // Assert
+      Assert.Equal(string.Empty, result);
+    }
+
+    [Fact]
+    public void GetValidationResults_SholdNotReturn_ErrorValidationResult()
+    {
+      // Arrange
+      var validator = Substitute.ForPartsOf<NugetToolPresenceValidatorTests>();
+      validator.When(a => a.GetScript(Arg.Any<string>())).DoNotCallBase();
+      validator.When(a => a.GetScriptError(Arg.Any<string>())).DoNotCallBase();
+      validator.GetScriptError(Arg.Any<string>()).Returns(string.Empty);
+
+      // Act
+      var result = validator.GetValidationResults();
+
+      // Assert
+      Assert.False(result.Any());
+    }
+
+    [Fact]
+    public void GetValidationResults_ShouldReturn_ErrorValidationResult()
+    {
+      // Arrange
+      var validator = Substitute.ForPartsOf<NugetToolPresenceValidatorTests>();
+      validator.When(a => a.GetScript(Arg.Any<string>())).DoNotCallBase();
+      validator.When(a => a.GetScriptError(Arg.Any<string>())).DoNotCallBase();
+      validator.GetScriptError(Arg.Any<string>()).Returns("CommandNotFoundException");
+
+      // Act
+      var result = validator.GetValidationResults();
+
+      // Assert
+      Assert.True(result.Any());
+    }
+  }
+}
---
+++ src/SIM.Sitecore9Installer/Validation/Validators/NugetToolPresenceValidator.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Management.Automation;
+using SIM.Sitecore9Installer.Tasks;
+
+namespace SIM.Sitecore9Installer.Validation.Validators
+{
+  public class NugetToolPresenceValidator : BaseValidator
+  {
+    protected virtual string TaskName => "sitecore-XP1-cm-dds-patch";
+
+    protected virtual string CommandToEvaluate => "nuget.exe";
+
+    protected override IEnumerable<ValidationResult> GetErrorsForTask(Task task, IEnumerable<InstallParam> paramsToValidate)
+    {
+      if (task.Name.Equals(TaskName, System.StringComparison.InvariantCultureIgnoreCase))
+      {
+        IEnumerable<ValidationResult> result = GetValidationResults();
+
+        return result;
+      }
+
+      return new ValidationResult[0];
+    }
+
+    protected virtual string GetScript(string command)
+    {
+      // Try to run 'nuget.exe' tool without any parameters.
+      // In case the tool is absent the CommandNotFoundException is thrown.
+
+      string script = command;
+
+      return script;
+    }
+
+    protected virtual IEnumerable<ValidationResult> GetValidationResults()
+    {
+      string script = GetScript(CommandToEvaluate);
+
+      string psError = GetScriptError(script);
+
+      if (psError.Equals("CommandNotFoundException", System.StringComparison.InvariantCultureIgnoreCase))
+      {
+        ValidationResult result = new ValidationResult(
+          state: ValidatorState.Error,
+          message: $"{TaskName}: nuget.exe is not a recognized as an internal or external command. To fix: download the NuGet.exe from https://www.nuget.org/downloads and set the local path of NuGet.exe into your path environment variable.",
+          error: null);
+
+        yield return result;
+      }
+      else if (!string.IsNullOrEmpty(psError))
+      {
+        ValidationResult result = new ValidationResult(
+          state: ValidatorState.Error,
+          message: $"{TaskName}: The '{psError}' error occurred when 'nuget.exe' was invoked from PowerShell.",
+          error: null);
+
+        yield return result;
+      }
+    }
+
+    protected virtual string GetScriptError(string script)
+    {
+      using (PowerShell PowerShellInstance = PowerShell.Create())
+      {
+        PowerShellInstance.AddScript(script);
+        PowerShellInstance.Invoke();
+
+        if (PowerShellInstance.Streams.Error.Count > 0)
+        {
+          return PowerShellInstance.Streams.Error[0]?.FullyQualifiedErrorId ?? string.Empty;
+        }
+        else
+        {
+          return string.Empty;
+        }
+      }
+    }
+  }
+}
