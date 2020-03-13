064f3d760063cef872d167fbd3c60e457f1a3567
Ivan Sheyenko <ivs@sitecore.net>
2020-03-13 15:49:32 -0400

    sql version validator. #379

---
+++ src/SIM.Sitecore9Installer/Validation/Validators/SqlVersionValidator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Task = SIM.Sitecore9Installer.Tasks.Task;
+
+namespace SIM.Sitecore9Installer.Validation.Validators
+{
+  public class SqlVersionValidator:IValidator
+  {
+    public IEnumerable<ValidationResult> Evaluate(IEnumerable<Task> tasks)
+    {
+      string server = this.Data["Server"];
+      string user = this.Data["User"];
+      string pass = this.Data["Password"];
+      foreach (Task task in tasks.Where(t => t.LocalParams.Any(p => p.Name == user)))
+      {
+        SqlConnection conn =
+          new SqlConnection($"Data Source={task.LocalParams.Single(p=>p.Name==server).Value};" +
+                            $"Initial Catalog=master;User ID={task.LocalParams.Single(p => p.Name == user).Value};" +
+                            $"Password={task.LocalParams.Single(p => p.Name == pass).Value}");
+        conn.Open();
+        try
+        {
+          string[] versions= Data["Versions"].Split(',');
+          if (!versions.Any(v => Regex.Match(conn.ServerVersion, v).Success))
+          {
+            yield return new ValidationResult(ValidatorState.Error, "SQL server version is not compatible", null);
+          }
+        }
+        finally
+        {
+          conn.Close();
+        }
+      }
+
+      yield return new ValidationResult(ValidatorState.Success, null, null);
+    }
+
+    public Dictionary<string, string> Data { get; set; }
+  }
+}
