f409499f4a4a0e98f061f704f022c06bcfe0b29c
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2020-04-17 10:13:58 +0300

    #328 Add validators for the SiteName parameter of CM, DDS Patch and Identity Server

---
+++ src/SIM.Sitecore9Installer/Validation/Validators/CmDdsPatchSiteNameValidator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SIM.Sitecore9Installer.Tasks;
+
+namespace SIM.Sitecore9Installer.Validation.Validators
+{
+  public class CmDdsPatchSiteNameValidator : IValidator
+  {
+    private const string SitecoreXp1Cm = "sitecore-xp1-cm";
+
+    private const string SitecoreXp1CmDdsPatch = "sitecore-XP1-cm-dds-patch";
+
+    private const string SiteName = "SiteName";
+
+    private const string DdsPatchValidationResultMessage =
+      "Value of the '{0}' parameter differs in the '{1}' and '{2}' tasks. To fix: set the same value for each mentioned task in Advanced installation parameters.";
+
+    public Dictionary<string, string> Data { get; set; }
+
+    public CmDdsPatchSiteNameValidator()
+    {
+      this.Data = new Dictionary<string, string>();
+    }
+
+    public IEnumerable<ValidationResult> Evaluate(IEnumerable<Task> tasks)
+    {
+      Task cmTask = tasks.Single(t => t.Name.Equals(SitecoreXp1Cm, StringComparison.InvariantCultureIgnoreCase) && 
+                                      t.LocalParams.Any(p => p.Name.Equals(SiteName, StringComparison.InvariantCultureIgnoreCase)));
+
+      if (cmTask != null)
+      {
+        Task ddsPatchTask = tasks.Single(t => t.Name.Equals(SitecoreXp1CmDdsPatch, StringComparison.InvariantCultureIgnoreCase) &&
+                                              t.LocalParams.Any(p => p.Name.Equals(SiteName, StringComparison.InvariantCultureIgnoreCase)));
+
+        if (ddsPatchTask != null)
+        {
+          if (cmTask.LocalParams.Single(p => p.Name.Equals(SiteName, StringComparison.InvariantCultureIgnoreCase)).Value != 
+              ddsPatchTask.LocalParams.Single(p => p.Name.Equals(SiteName, StringComparison.InvariantCultureIgnoreCase)).Value)
+          {
+            yield return new ValidationResult(ValidatorState.Error,
+              string.Format(DdsPatchValidationResultMessage, SiteName, cmTask.Name, ddsPatchTask.Name), null);
+          }
+        }
+      }
+
+      yield return new ValidationResult(ValidatorState.Success, null, null);
+    }
+  }
+}
---
+++ src/SIM.Sitecore9Installer/Validation/Validators/CmIdentityServerSiteNameValidator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SIM.Sitecore9Installer.Tasks;
+
+namespace SIM.Sitecore9Installer.Validation.Validators
+{
+  public class CmIdentityServerSiteNameValidator : IValidator
+  {
+    private const string SitecoreXp1Cm = "sitecore-xp1-cm";
+
+    private const string SitecoreXm1Cm = "Sitecore-xm1-cm";
+
+    private const string SitecoreXm0 = "Sitecore-XP0";
+
+    private const string SiteName = "SiteName";
+
+    private const string IdentityServer = "IdentityServer";
+
+    private const string AllowedCorsOrigins = "AllowedCorsOrigins";
+
+    private const string PasswordRecoveryUrl = "PasswordRecoveryUrl";
+
+    private const string IdentityServerValidationResultMessage =
+      "The '{0}' parameter of the '{1}' task doesn't match the '{2}' parameter of the '{3}' task. To fix: set the correct value of the mentioned parameter in Advanced installation parameters.";
+
+
+    public Dictionary<string, string> Data { get; set; }
+
+    public CmIdentityServerSiteNameValidator()
+    {
+      this.Data = new Dictionary<string, string>();
+    }
+
+    public IEnumerable<ValidationResult> Evaluate(IEnumerable<Task> tasks)
+    {
+      Task cmTask = tasks.Single(t => (t.Name.Equals(SitecoreXp1Cm, StringComparison.InvariantCultureIgnoreCase) || 
+                                       t.Name.Equals(SitecoreXm1Cm, StringComparison.InvariantCultureIgnoreCase) ||
+                                       t.Name.Equals(SitecoreXm0, StringComparison.InvariantCultureIgnoreCase)) && 
+                                      t.LocalParams.Any(p => p.Name.Equals(SiteName, StringComparison.InvariantCultureIgnoreCase)));
+
+      if (cmTask != null)
+      {
+        string cmSiteName = cmTask.LocalParams.Single(p => p.Name.Equals(SiteName, StringComparison.InvariantCultureIgnoreCase)).Value;
+
+        Task identityServerTask = tasks.Single(t => t.Name.Equals(IdentityServer, StringComparison.InvariantCultureIgnoreCase) &&
+                                                    t.LocalParams.Any(p => p.Name.Equals(AllowedCorsOrigins, StringComparison.InvariantCultureIgnoreCase)) &&
+                                                    t.LocalParams.Any(p => p.Name.Equals(PasswordRecoveryUrl, StringComparison.InvariantCultureIgnoreCase)));
+
+        if (identityServerTask != null)
+        {
+          Uri allowedCorsOriginsUri = new Uri(identityServerTask.LocalParams.Single(p => p.Name.Equals(AllowedCorsOrigins, StringComparison.InvariantCultureIgnoreCase)).Value);
+          Uri passwordRecoveryUrlUri = new Uri(identityServerTask.LocalParams.Single(p => p.Name.Equals(PasswordRecoveryUrl, StringComparison.InvariantCultureIgnoreCase)).Value);
+
+          if (cmSiteName != allowedCorsOriginsUri.Host)
+          {
+            yield return new ValidationResult(ValidatorState.Warning,
+              string.Format(IdentityServerValidationResultMessage, AllowedCorsOrigins, identityServerTask.Name, SiteName, cmTask.Name), null);
+          }
+
+
+          if (cmSiteName != passwordRecoveryUrlUri.Host)
+          {
+            yield return new ValidationResult(ValidatorState.Warning,
+              string.Format(IdentityServerValidationResultMessage, PasswordRecoveryUrl, identityServerTask.Name, SiteName, cmTask.Name), null);
+          }
+        }
+      }
+
+      yield return new ValidationResult(ValidatorState.Success, null, null);
+    }
+  }
+}
