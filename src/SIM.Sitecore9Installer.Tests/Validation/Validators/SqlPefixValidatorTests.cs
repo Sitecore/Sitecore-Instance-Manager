8c85f8bb98cb987937d494275df1d34c3f941c87
Ivan Sheyenko <ivs@sitecore.net>
2020-04-17 13:42:11 -0400

    Update tests. #386

--- src/SIM.Sitecore9Installer.Tests/Validation/Validators/SqlPefixValidatorTests.cs
+++ src/SIM.Sitecore9Installer.Tests/Validation/Validators/SqlPefixValidatorTests.cs
@@ -1,4 +1,5 @@
-﻿using NSubstitute;
+﻿using AutoFixture;
+using NSubstitute;
 using SIM.Sitecore9Installer.Tasks;
 using SIM.Sitecore9Installer.Validation;
 using SIM.Sitecore9Installer.Validation.Validators;
@@ -13,54 +14,29 @@ namespace SIM.Sitecore9Installer.Tests.Validation.Validators
   public class SqlPefixValidatorTests
   {
     [Theory]
-    [ClassData(typeof(ValidatorTestSetup))]
-    public void NoDbsWithTheSamePrefix(IEnumerable<Task> tasks)
+    [InlineData("server","user","pass","prefix","sql-server","sql-user","sql-pass","db-prefix",new string[] {"somedb"},1)]
+    [InlineData("server", "user", "pass", "prefix", "sql-server", "sql-user", "sql-pass", "db-prefix", new string[] { }, 0)]
+    public void DbsWithTheSamePrefix(string server, string user, string pass, string prefix, 
+      string serverValue, string userValue, string passValue, string prefixValue, string[] foundDbs, int errorsCount)
     {
       SqlPefixValidator val = Substitute.ForPartsOf<SqlPefixValidator>();
-      val.WhenForAnyArgs(v => v.GetDbList(null, null, null)).DoNotCallBase();
-      val.GetDbList(null, null, null).ReturnsForAnyArgs(new string[] { "someDb" });
-      string server = "SqlServer";
-      string user = "SqlAdminUser";
-      string pass = "SqlAdminPassword";
-      string prefix = "SqlDbPrefix";
+      val.WhenForAnyArgs(v => v.GetDbList(null, null, null, null)).DoNotCallBase();
+      val.GetDbList(null, null, null, null).ReturnsForAnyArgs(foundDbs);      
       val.Data["Server"] = server;
       val.Data["User"] = user;
       val.Data["Password"] = pass;
       val.Data["Prefix"] = prefix;
-      Task t = tasks.First();
-      t.LocalParams.Add(new InstallParam(server, "sql-server"));
-      t.LocalParams.Add(new InstallParam(user, "sql-user"));
-      t.LocalParams.Add(new InstallParam(pass, "sql-pass"));
-      t.LocalParams.Add(new InstallParam(prefix, "sql-prefix"));
-
-      IEnumerable<ValidationResult> results = val.Evaluate(tasks);
-      Assert.DoesNotContain(results, r => r.State != Sitecore9Installer.Validation.ValidatorState.Success);
-
-    }
-
-    [Theory]
-    [ClassData(typeof(ValidatorTestSetup))]
-    public void DbsWithTheSamePrefix(IEnumerable<Task> tasks)
-    {
-      SqlPefixValidator val = Substitute.ForPartsOf<SqlPefixValidator>();
-      val.WhenForAnyArgs(v => v.GetDbList(null, null, null)).DoNotCallBase();      
-      string server = "SqlServer";
-      string user = "SqlAdminUser";
-      string pass = "SqlAdminPassword";
-      string prefix = "SqlDbPrefix";
-      val.Data["Server"] = server;
-      val.Data["User"] = user;
-      val.Data["Password"] = pass;
-      val.Data["Prefix"] = prefix;
-      Task t = tasks.First();
-      t.LocalParams.Add(new InstallParam(server, "sql-server"));
-      t.LocalParams.Add(new InstallParam(user, "sql-user"));
-      t.LocalParams.Add(new InstallParam(pass, "sql-pass"));
-      t.LocalParams.Add(new InstallParam(prefix, "sql-prefix"));
-      val.GetDbList(null, null, null).ReturnsForAnyArgs(new string[] { "sql-prefixDatabase","SomeOtherDb" });
-      IEnumerable<ValidationResult> results = val.Evaluate(tasks);
-      Assert.Contains(results, r => r.State == Sitecore9Installer.Validation.ValidatorState.Error);
-
-    }
+      Fixture _fix = new Fixture();
+      Task t = Substitute.For<Task>(_fix.Create<string>(), _fix.Create<int>(), null, new List<InstallParam>(), new Dictionary<string, string>());
+      t.GlobalParams.Returns(new List<InstallParam>());
+      t.LocalParams.Returns(new List<InstallParam>());
+      t.LocalParams.Add(new InstallParam(server, serverValue));
+      t.LocalParams.Add(new InstallParam(user, userValue));
+      t.LocalParams.Add(new InstallParam(pass, passValue));
+      t.LocalParams.Add(new InstallParam(prefix, prefixValue));
+      //act
+      IEnumerable<ValidationResult> results = val.Evaluate(new Task[] { t});
+      Assert.Equal(errorsCount,results.Count(r => r.State == Sitecore9Installer.Validation.ValidatorState.Error));
+    }  
   }
 }
