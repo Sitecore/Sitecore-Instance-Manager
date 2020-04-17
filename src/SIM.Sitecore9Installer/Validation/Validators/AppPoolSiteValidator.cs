caabc5112a6817f7191af0f2dd59f30b9e96258c
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2020-04-17 14:49:38 +0300

    #411 Add validation to check if app pools and sites already exist in IIS

---
+++ src/SIM.Sitecore9Installer/Validation/Validators/AppPoolSiteValidator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Web.Administration;
+using SIM.Sitecore9Installer.Tasks;
+
+namespace SIM.Sitecore9Installer.Validation.Validators
+{
+  public class AppPoolSiteValidator : IValidator
+  {
+    protected virtual string SiteName => "SiteName";
+
+    protected virtual string AppPoolValidationMessage => "The '{0}' app pool already exists in IIS. To fix: change the '{1}' field's value of the '{2}' task in Advanced installation parameters.";
+
+    protected virtual string SiteValidationMessage => "The '{0}' site already exists in IIS. To fix: change the '{1}' field's value of the '{2}' task in Advanced installation parameters.";
+
+    public Dictionary<string, string> Data { get; set; }
+
+    public AppPoolSiteValidator()
+    {
+      this.Data = new Dictionary<string, string>();
+    }
+
+    public IEnumerable<ValidationResult> Evaluate(IEnumerable<Task> tasks)
+    {
+      foreach (Task task in tasks.Where(t => t.LocalParams.Any(p => p.Name.Equals(this.SiteName, StringComparison.InvariantCultureIgnoreCase))))
+      {
+        string siteNameParam = task.LocalParams.Single(p => p.Name.Equals(this.SiteName, StringComparison.InvariantCultureIgnoreCase)).Value;
+
+        if (this.AppPoolExists(siteNameParam))
+        {
+          yield return new ValidationResult(ValidatorState.Error, string.Format(this.AppPoolValidationMessage, siteNameParam, this.SiteName, task.Name), null);
+        }
+
+        if (this.SiteExists(siteNameParam))
+        {
+          yield return new ValidationResult(ValidatorState.Error, string.Format(this.SiteValidationMessage, siteNameParam, this.SiteName, task.Name), null);
+        }
+      }
+
+      yield return new ValidationResult(ValidatorState.Success, null, null);
+    }
+
+    protected internal virtual bool AppPoolExists(string name)
+    {
+      using (ServerManager manager = new ServerManager())
+      {
+        return manager.ApplicationPools.Any(t => t.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+      }
+    }
+
+    protected internal virtual bool SiteExists(string name)
+    {
+      using (ServerManager manager = new ServerManager())
+      {
+        return manager.Sites.Any(t => t.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+      }
+    }
+  }
+}
