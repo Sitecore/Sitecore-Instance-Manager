10938663481fc545c3943290f35efc2017066964
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2020-04-22 12:02:05 +0300

    #411 Add unit test to validate error messages

--- src/SIM.Sitecore9Installer.Tests/Validation/Validators/AppPoolSiteValidatorTests.cs
+++ src/SIM.Sitecore9Installer.Tests/Validation/Validators/AppPoolSiteValidatorTests.cs
@@ -15,12 +15,16 @@ namespace SIM.Sitecore9Installer.Tests.Validation.Validators
 
     private const string SiteName = "SiteName";
 
+    private const string AppPoolValidationMessage = "The '{0}' app pool already exists in IIS. To fix: change the '{1}' field's value of the '{2}' task in Advanced installation parameters.";
+
+    private const string SiteValidationMessage = "The '{0}' site already exists in IIS. To fix: change the '{1}' field's value of the '{2}' task in Advanced installation parameters.";
+
     [Theory]
-    [InlineData("sc.local", true, true, 2)]
-    [InlineData("sc.local", false, true, 1)]
-    [InlineData("sc.local", true, false, 1)]
-    [InlineData("sc.local", false, false, 0)]
-    public void EvaluateTests(string taskSiteName, bool appPoolExists, bool siteExists, int errorsCount)
+    [InlineData("sc.local", true, true, 2, AppPoolValidationMessage, SiteValidationMessage)]
+    [InlineData("sc.local", false, true, 1, "", SiteValidationMessage)]
+    [InlineData("sc.local", true, false, 1, AppPoolValidationMessage, "")]
+    [InlineData("sc.local", false, false, 0, "", "")]
+    public void EvaluateTests(string taskSiteName, bool appPoolExists, bool siteExists, int errorsCount, string appPoolValidationMessage, string siteValidationMessage)
     {
       // Arrange 
       var fixture = new Fixture();
@@ -36,7 +40,6 @@ namespace SIM.Sitecore9Installer.Tests.Validation.Validators
       var validator = Substitute.ForPartsOf<AppPoolSiteValidator>();
       validator.AppPoolExists(taskSiteName).Returns(appPoolExists);
       validator.SiteExists(taskSiteName).Returns(siteExists);
-      validator.Data["SiteName"] = SiteName;
 
       // Act
       IEnumerable<ValidationResult> result = validator.Evaluate(new Task[] { task });
@@ -44,6 +47,17 @@ namespace SIM.Sitecore9Installer.Tests.Validation.Validators
 
       // Assert
       Assert.Equal(errors.Count(), errorsCount);
+      this.ValidateMessage(appPoolValidationMessage, taskSiteName, SiteName, task.Name, errors);
+      this.ValidateMessage(siteValidationMessage, taskSiteName, SiteName, task.Name, errors);
+    }
+
+    private void ValidateMessage(string validationMessage, string paramValue, string paramName, string taskName, IEnumerable<ValidationResult> errors)
+    {
+      if (!string.IsNullOrEmpty(validationMessage))
+      {
+        validationMessage = string.Format(validationMessage, paramValue, paramName, taskName);
+        Assert.Contains(errors, error => error.Message.Equals(validationMessage));
+      }
     }
   }
 }
