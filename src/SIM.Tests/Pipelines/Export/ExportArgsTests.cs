bd0c793def174fd8e11d3c5ff503d7080f533d1e
KirillVolovitskiy <kirill.volovitskiy@sitecore.com>
2020-09-04 11:46:49 +0300

    #377: Use single method to get drive in ExportArgsTests

--- src/SIM.Tests/Pipelines/Export/ExportArgsTests.cs
+++ src/SIM.Tests/Pipelines/Export/ExportArgsTests.cs
@@ -12,11 +12,7 @@
     [Fact]
     public void GetTempFolderTest()
     {
-      //Try to get disk C. Get any disk if it doesn't exist
-      var drive = Environment.GetLogicalDrives()
-        .FirstOrDefault(d => d.EqualsIgnoreCase("c:\\")) 
-        ?? Environment.GetLogicalDrives().First();
-
+      var drive = GetDrive();
       var result = ExportArgs.GetTempFolder(null, $"{drive}inetpub\\wwwroot");
 
       // to eliminate random part
@@ -28,17 +24,24 @@
     [Fact]
     public void GetTempFolderTest_Custom()
     {
-      //Try to get disk C. Get any disk if it doesn't exist
-      var drive = Environment.GetLogicalDrives()
-        .FirstOrDefault(d => d.EqualsIgnoreCase("c:\\"))
-        ?? Environment.GetLogicalDrives().First();
-
+      var drive = GetDrive();
       var result = ExportArgs.GetTempFolder($"{drive}Sitecore\\Temp", @"D:\inetpub\wwwroot");
 
       // to eliminate random part
       var actual = Path.GetDirectoryName(result);
 
       Assert.Equal($"{drive}Sitecore\\Temp", actual);
+    }
+
+    private string GetDrive()
+    {
+      //Try to get disk C as it is quite common. 
+      //Get any disk if it doesn't exist.
+      var drive = Environment.GetLogicalDrives()
+        .FirstOrDefault(d => d.EqualsIgnoreCase("c:\\"))
+        ?? Environment.GetLogicalDrives().First();
+
+      return drive;
     }
   }
 }
