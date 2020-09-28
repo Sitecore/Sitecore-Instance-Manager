be9fb7a4508d292f43025b9bc8468b7d01fea026
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2020-09-28 10:27:51 +0300

    #400 Use SCLA ClickOnce app

--- src/SIM.Tool.Windows/MainWindowComponents/OpenLogsButton.cs
+++ src/SIM.Tool.Windows/MainWindowComponents/OpenLogsButton.cs
@@ -1,40 +1,14 @@
 ﻿namespace SIM.Tool.Windows.MainWindowComponents
 {
-  using System.IO;
   using System.Windows;
-  using SIM.Core.Common;
   using SIM.Instances;
   using SIM.Tool.Base.Plugins;
   using JetBrains.Annotations;
-  using SIM.Extensions;
+  using SIM.Core;
 
   [UsedImplicitly]
-  public class OpenLogsButton : AbstractDownloadAndRunButton, IMainWindowButton
+  public class OpenLogsButton : IMainWindowButton
   {
-    protected override string BaseUrl
-    {
-      get
-      {
-        return "http://dl.sitecore.net/updater/1.1/scla/";
-      }
-    }
-
-    protected override string AppName
-    {
-      get
-      {
-        return "Log Analyzer";
-      }
-    }
-
-    protected override string ExecutableName
-    {
-      get
-      {
-        return "SitecoreLogAnalyzer.exe";
-      }
-    }
-
     #region Public methods
 
     public bool IsEnabled(Window mainWindow, Instance instance)
@@ -47,20 +21,9 @@
       return MainWindowHelper.IsEnabledOrVisibleButtonForSitecoreMember(instance);
     }
 
-    public override void OnClick(Window mainWindow, Instance instance)
+    public void OnClick(Window mainWindow, Instance instance)
     {
-      if (instance != null)
-      {
-        var dataFolderPath = instance.DataFolderPath;
-        FileSystem.FileSystem.Local.Directory.AssertExists(dataFolderPath, "The data folder ({0}) of the {1} instance doesn't exist".FormatWith(dataFolderPath, instance.Name));
-
-        var logs = Path.Combine(dataFolderPath, "logs");
-        RunApp(mainWindow, logs);
-
-        return;
-      }
-
-      RunApp(mainWindow);
+      CoreApp.RunApp("iexplore", "http://dl.sitecore.net/updater/scla/SitecoreLogAnalyzer.application");
     }
 
     #endregion
