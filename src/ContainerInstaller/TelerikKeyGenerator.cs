5e91704d41065b17658cee35a7393e3e0cb628cd
Andrey Filchenkov <22079019+AndreyFilchenkov@users.noreply.github.com>
2020-12-07 22:23:35 +0200

    Update namespace to SIM.ContainerInstaller

--- src/ContainerInstaller/TelerikKeyGenerator.cs
+++ src/ContainerInstaller/TelerikKeyGenerator.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Management.Automation;
 using JetBrains.Annotations;
 
-namespace ContainerInstaller
+namespace SIM.ContainerInstaller
 {
   [UsedImplicitly]
   public class TelerikKeyGenerator : ITelerikKeyGenerator
