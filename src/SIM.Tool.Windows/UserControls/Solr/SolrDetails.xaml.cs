eed4bcdb6cc792be7c51282377da484ff599e8c7
Viacheslav Afanasyev <9393016+ViacheslavAfanasyev@users.noreply.github.com>
2021-02-24 17:01:12 -0500

    #Add checking jave.exe file in the variable path

--- src/SIM.Tool.Windows/UserControls/Solr/SolrDetails.xaml.cs
+++ src/SIM.Tool.Windows/UserControls/Solr/SolrDetails.xaml.cs
@@ -77,6 +77,11 @@ namespace SIM.Tool.Windows.UserControls.Install
         MessageBox.Show("Required JAVA_HOME system variable was not found.\nIt seems Java Runtime Environment (JRE) has not been installed.", "Warning");
         return false;
       }
+      else if(!Directory.Exists(Path.Combine(javaHomeVariable, "bin")) || Directory.GetFiles(Path.Combine(javaHomeVariable, "bin"),"java.exe").Length==0)
+      {
+        MessageBox.Show($"Java Runtime Environment (JRE) was not found.\nMissing file: {Path.Combine(javaHomeVariable, "bin", "java.exe")}", "Warning");
+        return false;
+      }
 
       tasker.GlobalParams.First(p => p.Name == "JavaHome").Value = javaHomeVariable;
       Install9WizardArgs args = (Install9WizardArgs)wizardArgs;
