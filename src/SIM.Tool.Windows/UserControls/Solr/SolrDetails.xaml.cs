ed1b00ef2785e46ff82ba9f855eb2634b13a4a7a
Viacheslav Afanasyev <9393016+ViacheslavAfanasyev@users.noreply.github.com>
2021-02-26 14:35:34 -0500

    #Change warn message

--- src/SIM.Tool.Windows/UserControls/Solr/SolrDetails.xaml.cs
+++ src/SIM.Tool.Windows/UserControls/Solr/SolrDetails.xaml.cs
@@ -77,9 +77,9 @@ namespace SIM.Tool.Windows.UserControls.Install
         MessageBox.Show("Required JAVA_HOME system variable was not found.\nIt seems Java Runtime Environment (JRE) has not been installed.", "Warning");
         return false;
       }
-      else if(!Directory.Exists(Path.Combine(javaHomeVariable, "bin")) || Directory.GetFiles(Path.Combine(javaHomeVariable, "bin"),"java.exe").Length==0)
+      else if(!Directory.Exists(Path.Combine(javaHomeVariable, "bin1")) || Directory.GetFiles(Path.Combine(javaHomeVariable, "bin"),"java.exe").Length==0)
       {
-        MessageBox.Show($"Java Runtime Environment (JRE) was not found.\nMissing file: {Path.Combine(javaHomeVariable, "bin", "java.exe")}", "Warning");
+        MessageBox.Show($"The 'JAVA_HOME' system variable does not point to the proper JRE folder: {javaHomeVariable}", "Warning");
         return false;
       }
 
