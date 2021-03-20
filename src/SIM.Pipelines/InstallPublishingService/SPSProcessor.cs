e446ee062593dc2ada5350b8f8d30e2511156dc6
Tyler Jass <tja@sitecore.net>
2021-03-20 17:13:29 -0400

    updated abstract SPSProcessor class

--- src/SIM.Pipelines/InstallPublishingService/SPSProcessor.cs
+++ src/SIM.Pipelines/InstallPublishingService/SPSProcessor.cs
@@ -1,43 +1,15 @@
 ﻿using JetBrains.Annotations;
 using SIM.Pipelines.Processors;
-using Sitecore.Diagnostics.Logging;
-using System;
 
 namespace SIM.Pipelines.InstallPublishingService
 {
   public abstract class SPSProcessor<T> : Processor where T : ProcessorArgs
   {
-    protected static bool AbortPipeline = false;
-
     protected override void Process([NotNull] ProcessorArgs args)
-    {
-      T processorArgs = args as T;
-
-      if (AbortPipeline)
-      {
-        Log.Warn($"The {this.GetType().Name} processor was skipped.");
-        this.Skip();
-        return;
-      }
-
-      try
-      {
-        this.ProcessCore(processorArgs);
-      }
-      catch (Exception ex)
-      {
-        AbortPipeline = true;
-        Log.Error($"Publishing Service Pipeline was aborted.  The remaining steps were skipped.");
-        throw ex;
-      }
+    { 
+      this.ProcessCore((T)args);
     }
 
     protected abstract void ProcessCore([NotNull] T args);
-
-    //Kind of a hack so I can resure some of the uninstall sps processors in the RemoveExistingSPSProcessor class
-    internal void DoProcess([NotNull] T args)
-    {
-      this.Process(args);
-    }
   }
 }
