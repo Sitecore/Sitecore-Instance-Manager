c18a87e41ea74e0450fc48bbba307cabb2f40cb2
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2022-01-21 14:52:05 +0200

    #640 Improve deletion of Sitecore 8 and earlier

--- src/SIM.Tool.Windows/MainWindowComponents/Buttons/MultipleDeletionButton.cs
+++ src/SIM.Tool.Windows/MainWindowComponents/Buttons/MultipleDeletionButton.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using JetBrains.Annotations;
+using SIM.Instances;
 using SIM.Pipelines.MultipleDeletion;
 using SIM.Tool.Base.Wizards;
 using SIM.Tool.Windows.UserControls.MultipleDeletion;
@@ -14,16 +16,31 @@ namespace SIM.Tool.Windows.MainWindowComponents.Buttons
 
     protected override void OnClick(Window mainWindow)
     {
-      WizardPipelineManager.Start("multipleDeletion", mainWindow, new MultipleDeletionArgs(new List<string>()), null, ignore => OnWizardCompleted(), () => new MultipleDeletionWizardArgs());
+      if (InstanceManager.Default.Instances != null)
+      {
+        IOrderedEnumerable<Instance> instances = InstanceManager.Default.Instances.Where(instance => instance.Type == Instance.InstanceType.Sitecore8AndEarlier).OrderBy(instance => instance.Name);
+        WizardPipelineManager.Start("multipleDeletion", mainWindow, new MultipleDeletionArgs(new List<string>()), null, wizardArgs => OnWizardCompleted(wizardArgs), () => new MultipleDeletionWizardArgs() { _Instances = instances });
+      }
+      else
+      {
+        if (MessageBox.Show("Sitecore instances cannot be loaded. Would you like to refresh Sitecore web sites and repeat the action?", "Multiple deletion Sitecore 8 and earlier", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+        {
+          MainWindowHelper.RefreshInstances();
+          this.OnClick(mainWindow);
+        }
+      }
     }
 
     #endregion
 
     #region Private methods
 
-    private static void OnWizardCompleted()
+    private static void OnWizardCompleted(WizardArgs wizardArgs)
     {
-      MainWindowHelper.SoftlyRefreshInstances();
+      if (wizardArgs.ShouldRefreshInstancesList)
+      {
+        MainWindowHelper.SoftlyRefreshInstances();
+      }
     }
 
     #endregion
