db54ce78e4b952d34bb7bb6bde9991ec43433014
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2023-11-14 12:33:23 +0200

    #755 Update the license validator to work correctly when changing the license

--- src/SIM.Sitecore9Installer.Tests/Validation/Validators/LicenseFileValidatorTests.cs
+++ src/SIM.Sitecore9Installer.Tests/Validation/Validators/LicenseFileValidatorTests.cs
@@ -11,6 +11,10 @@ namespace SIM.Sitecore9Installer.Tests.Validation.Validators
 {
   public class LicenseFileValidatorTests
   {
+    private const string LicenseFileName = "LicenseFile";
+    private const string LicenseFilePath = @"C:\license.xml";
+    private const string DateFormat = "yyyyMMdd";
+
     [Theory]
     [InlineData(true, ValidatorState.Success, false)]
     [InlineData(false, ValidatorState.Error, false)]
@@ -23,27 +27,34 @@ namespace SIM.Sitecore9Installer.Tests.Validation.Validators
       GlobalParameters globals = new GlobalParameters();
       task.LocalParams.Returns(new LocalParameters(new List<InstallParam>(), globals));
 
-      task.LocalParams.AddOrUpdateParam("LicenseFile", @"C:\license.xml", InstallParamType.String);
+      task.LocalParams.AddOrUpdateParam(LicenseFileName, LicenseFilePath, InstallParamType.String);
 
       List<Task> tasks = Substitute.For<List<Task>>();
       tasks.Add(task);
 
       LicenseFileValidator val = Substitute.ForPartsOf<LicenseFileValidator>();
       val.FileExists(string.Empty).ReturnsForAnyArgs(fileExists);
-      val.GetXmlFileNodes(string.Empty, string.Empty).ReturnsForAnyArgs((XmlNodeList)null);
-      val.Data["LicenseFileVariable"] = "LicenseFile";
 
+      string date = DateTime.Now.AddYears(1).ToString(DateFormat);
       if (useExpiredDate)
       {
         if (expectedResult == ValidatorState.Error)
         {
-          val.ExpiredDatesAndLicences.Add(DateTime.Now.AddYears(-1).ToShortDateString(), new List<string>() { "SiteCore.License" });
+          date = DateTime.Now.AddYears(-1).ToString(DateFormat);
         }
         else if (expectedResult == ValidatorState.Warning)
         {
-          val.AlmostExpiredDatesAndLicences.Add(DateTime.Now.AddMonths(-1).ToShortDateString(), new List<string>() { "SiteCore.License" });
+          date = DateTime.Now.AddMonths(1).ToString(DateFormat);
         }
       }
+
+      string license = "<?xml version=\"1.0\" encoding=\"utf-16\"?><?xml-stylesheet type=\"text/xsl\" href=\"http://www.sitecore.net/licenseviewer/license.xsl\"?>" +
+        "<signedlicense id=\"00000000000000\"><Signature><Object Id=\"SiteCore.License\" xmlns=\"http://www.w3.org/2000/09/xmldsig#\">" +
+        $"<license xmlns=\"\"><expiration>{date}T000000</expiration></license></Object></Signature></signedlicense>";
+      XmlDocument xmlDocument = new XmlDocument();
+      xmlDocument.LoadXml(license);
+      val.GetXmlFileNodes(string.Empty, string.Empty).ReturnsForAnyArgs(xmlDocument.GetElementsByTagName("expiration"));
+      val.Data["LicenseFileVariable"] = LicenseFileName;
 
       //Act
       IEnumerable<ValidationResult> results = val.Evaluate(tasks);
