66cc1b331c7c96089e0476238fddecc60f907650
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2024-03-18 12:49:23 +0200

    #777 Update the script to use AzureSignTool

--- build/PostCompile.ps1
+++ build/PostCompile.ps1
@@ -10,8 +10,7 @@ Param(
     [Parameter(Mandatory=$True)] [string]$BaseApplicationURL,
     [Parameter(Mandatory=$True)] [string]$SupportURL,
     [Parameter(Mandatory=$True)] [string]$PublisherName,
-    [Parameter(Mandatory=$True)] [string]$CertificatePath,
-    [Parameter(Mandatory=$True)] [string]$CertificatePassword
+	[Parameter(Mandatory=$True)] [string]$AzureSignToolFile
 )
 
 # get version
@@ -36,6 +35,9 @@ $ApplicationFile = "$OutputFolder\$ApplicationFileName"
 $URL = "$BaseApplicationUrl/$ApplicationFileName"
 $ExecutableFile = "$ReleaseFolder\$Name.exe"
 $ManifestFile = "$ExecutableFile.manifest"
+$MageToolFile = ".\tools\mage.exe"
+$TimestampSha2 = "http://timestamp.globalsign.com/?signature=sha2"
+$TimestampRfc3161 = "http://rfc3161timestamp.globalsign.com/advanced"
 
 # create parent folder of $ReleaseFolder
 MKDIR $ReleaseFolder
@@ -49,35 +51,23 @@ Move-Item $BuildFolder -Destination $ReleaseFolder
 "" | Set-Content "stderr.txt"
 "" | Set-Content "stdout.txt"
 
-# sign executable file
-Write-Host "> tools\signtool.exe sign /f `"$CertificatePath`" /p `"$CertificatePassword`" /t `"http://timestamp.digicert.com`" `"$ExecutableFile`""
-Start-Process -FilePath "tools\signtool.exe" -ArgumentList "sign /f `"$CertificatePath`" /p `"$CertificatePassword`" /t `"http://timestamp.digicert.com`" `"$ExecutableFile`"" -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
-Get-Content stdout.txt | Write-Host
-Get-Content stderr.txt | Write-Host
-
 # create manifest
-Write-Host "> tools\mage.exe -New Application -IconFile `"$Icon`" -ToFile `"$ManifestFile`" -FromDirectory `"$ReleaseFolder`" -Name `"$Name`" -Version `"$Version`" -Processor msil"
-Start-Process -FilePath "tools\mage.exe" -ArgumentList "-New Application -IconFile `"$Icon`" -ToFile `"$ManifestFile`" -FromDirectory `"$ReleaseFolder`" -Name `"$Name`" -Version `"$Version`" -Processor msil" -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
-Get-Content stdout.txt | Write-Host
-Get-Content stderr.txt | Write-Host
-
-# sign manifest
-Write-Host "> tools\mage.exe -Sign `"$ManifestFile`" -CertFile `"$CertificatePath`" -Password `"$CertificatePassword`""
-Start-Process -FilePath "tools\mage.exe" -ArgumentList "-Sign `"$ManifestFile`" -CertFile `"$CertificatePath`" -Password `"$CertificatePassword`"" -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
+Write-Host "> $MageToolFile -New Application -IconFile `"$Icon`" -ToFile `"$ManifestFile`" -FromDirectory `"$ReleaseFolder`" -Name `"$Name`" -Version `"$Version`" -Processor msil"
+Start-Process -FilePath $MageToolFile -ArgumentList "-New Application -IconFile `"$Icon`" -ToFile `"$ManifestFile`" -FromDirectory `"$ReleaseFolder`" -Name `"$Name`" -Version `"$Version`" -Processor msil" -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
 Get-Content stdout.txt | Write-Host
 Get-Content stderr.txt | Write-Host
 
 # create application file
-Write-Host "> tools\mage.exe -New Deployment -AppManifest `"$ManifestFile`" -ToFile `"$ApplicationFile`" -Processor msil -Install true -Publisher `"$PublisherName`" -ProviderUrl `"$URL`""
-Start-Process -FilePath "tools\mage.exe" -ArgumentList "-New Deployment -AppManifest `"$ManifestFile`" -ToFile `"$ApplicationFile`" -Processor msil -Install true -Publisher `"$PublisherName`" -ProviderUrl `"$URL`"" -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
+Write-Host "> $MageToolFile -New Deployment -AppManifest `"$ManifestFile`" -ToFile `"$ApplicationFile`" -Processor msil -Install true -Publisher `"$PublisherName`" -ProviderUrl `"$URL`""
+Start-Process -FilePath $MageToolFile -ArgumentList "-New Deployment -AppManifest `"$ManifestFile`" -ToFile `"$ApplicationFile`" -Processor msil -Install true -Publisher `"$PublisherName`" -ProviderUrl `"$URL`"" -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
 Get-Content stdout.txt | Write-Host
 Get-Content stderr.txt | Write-Host
 
 # workaround to replace some wrong values that I have no idea they came from and why + add a couple things mage doesn't seem to support (SupportUrl)
 (Get-Content $ApplicationFile).Replace("asmv2:product=`"SIM`"", "asmv2:product=`"$Title`" asmv2:supportUrl=`"$SupportUrl`"").Replace("name=`"SIM.app`" version=`"1.0.0.0`"", "name=`"$ApplicationFileName`" version=`"$Version`"").Replace("<expiration maximumAge=`"0`" unit=`"days`" />", "<expiration maximumAge=`"0`" unit=`"days`" />") | Set-Content $ApplicationFile
 
-# sign application file
-Write-Host "> tools\mage.exe -Sign `"$ApplicationFile`" -CertFile `"$CertificatePath`" -Password `"$CertificatePassword`""
-Start-Process -FilePath "tools\mage.exe" -ArgumentList "-Sign `"$ApplicationFile`" -CertFile `"$CertificatePath`" -Password `"$CertificatePassword`"" -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
+# sign executable file, manifest and application file
+Write-Host "> $AzureSignToolFile -path=$OutputFolder -mageToolPath=$MageToolFile -azure-key-vault-url=$env:AzureKeyvaultUrl -azure-key-vault-client-id=$env:AzureSigningClientId -azure-key-vault-client-secret=$env:AzureSigningSecret -azure-key-vault-tenant-id=$env:AzureSigningTenantId -azure-key-vault-certificate=$env:SignCertificateName -timestamp-sha2=$TimestampSha2 -timestamp-rfc3161=$TimestampRfc3161 -description=$Title"
+Start-Process -FilePath $AzureSignToolFile -ArgumentList "-path=$OutputFolder -mageToolPath=$MageToolFile -azure-key-vault-url=$env:AzureKeyvaultUrl -azure-key-vault-client-id=$env:AzureSigningClientId -azure-key-vault-client-secret=$env:AzureSigningSecret -azure-key-vault-tenant-id=$env:AzureSigningTenantId -azure-key-vault-certificate=$env:SignCertificateName -timestamp-sha2=$TimestampSha2 -timestamp-rfc3161=$TimestampRfc3161 -description=$Title" -Wait -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt
 Get-Content stdout.txt | Write-Host
 Get-Content stderr.txt | Write-Host
